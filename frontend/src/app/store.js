bfb142a76eae1fce488cdba9a2f6ce67e0cd40f8
ankitnayka <ankitnayka2802@gmail.com>
2025-03-03 16:27:55 +0530

    loging redux2

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,13 +2,15 @@ import {configureStore} from '@reduxjs/toolkit';
 import { authApi } from '@/utils/api/userApi';
 import { authCaptainApi } from '@/utils/api/captainApi';
 import userReducer from '../features/api/userSlice'
+import captainReducer from '../features/api/captainSlice'
 
 
 export const  store =configureStore({
     reducer:{
         [authApi.reducerPath]:authApi.reducer,
         [authCaptainApi.reducerPath]:authCaptainApi.reducer,
-        user:userReducer
+        user:userReducer,
+        captain:captainReducer
     },
     middleware:(getDefaultMiddleware)=>
         getDefaultMiddleware().concat(authApi.middleware,authCaptainApi.middleware)
